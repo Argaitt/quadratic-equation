a247efc3d1338037a4987118f944d6f42f294971
argaitt <argaitt@gmail.com>
2018-09-26 23:46:38 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let pointer = equation.indexOf(' ');
+  let a = + equation.substring(0, pointer).replace(/\s+/g, '');
+  let b =  + equation.substring(pointer + 7, equation.indexOf(' ', pointer+9)).replace(/\s+/g,'');
+  let c = + equation.substring(equation.lastIndexOf(' ') - 2).replace(/\s+/g, '');
+  let D = b * b - 4 * a * c;
+  let arrOfSolution = [];
+  arrOfSolution.push(Math.round((- b + Math.sqrt(D)) / (2 * a)));
+  arrOfSolution.push(Math.round((- b - Math.sqrt(D)) / (2 * a)));
+  return arrOfSolution.sort((a, b) => a - b);
 }
